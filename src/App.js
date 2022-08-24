9b5f8e64aed05a07d8f877e434d7de7dc866fd88
신 우현 <woo_hyun_1@naver.com>
2022-08-24 17:26:59 +0900

    Create Coin Tracker

--- src/App.js
+++ src/App.js
@@ -2,56 +2,33 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
-//컴포넌트는 JSX를 반환하는 함수이다 !! (무조껀 맨 앞글자는 대문자)
-function Hello() {
-  useEffect(() => {
-    console.log("Created :)");
-    //Cleanup Function : 컴포넌트가 destory, 즉 사라질 때 실행되는 함수
-    //컴포넌트가 생성될 때는 useEffect가, 사라질 때는 Cleanup Function(리턴 값)이 실행됨
-    return () => console.log("Destoryed :(");
-  }, []);
-  return <h1>Hello</h1>; //JSX
-}
-
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [...currentArray, toDo]); //modifer 함수의 사용법 첫번째 : 함수 전달(함수의 첫번째 인자로 현재 state를 전달)
-    setToDo(""); //modifer 함수의 사용법 첫번째 : 값 전달
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
 
-  console.log(toDos);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
-      {/*JS를 작성할 때는 중괄호를 열어줘야함 !!!*/}
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          type="text"
-          placeholder="Write your to do list"
-          onChange={onChange}
-        ></input>
-        <button>Add To Do</button>
-      </form>
-      <hr />
-
-      {/* map함수는 하나의 array에 있는 item을 내가 원하는 것으로 바꿔주고 그 item으로 새로운 array를 만들어 반환하는 역할 */}
-      {/* 즉, 이전의 array를 변형하여 새로운 array를 만드는 것 !! */}
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
@@ -65,5 +42,17 @@ function App() {
 
 // 🏴 부모 컴포넌트에서 리렌더링이 일어날 경우 모든 자식들이 리렌더링이 된다.(wa can use memo)
 // 🏴 propType을 설치하고 props의 타입을 지정해 줄 수 있다. 이 때 isRequired로 필수값을 지정 가능
+
+// 컴포넌트는 JSX를 반환하는 함수이다 !! (무조껀 맨 앞글자는 대문자)
+// JS를 작성할 때는 중괄호를 열어줘야함 !!!
+
+// Cleanup Function : 컴포넌트가 destory, 즉 사라질 때 실행되는 함수
+// 컴포넌트가 생성될 때는 useEffect가, 사라질 때는 Cleanup Function(리턴 값)이 실행됨
+
+// modifer 함수의 사용법 첫번째 : 값 전달
+// modifer 함수의 사용법 두번째 : 함수 전달(함수의 첫번째 인자로 현재 state를 전달)
+
+// map함수는 하나의 array에 있는 item을 내가 원하는 것으로 바꿔주고 그 item으로 새로운 array를 만들어 반환하는 역할
+// 즉, 이전의 array를 변형하여 새로운 array를 만드는 것 !!
 
 export default App;
